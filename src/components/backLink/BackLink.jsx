388c78b4deb25d7a5888b129feb4a4189fd9362a
Natalia <natalakrakoveckaya@gmail.com>
2023-03-04 11:43:28 -0500

    Summary

---
+++ src/components/backLink/BackLink.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import { StyledLink } from './BackLink.styled';
+
+export const BackLink = ({ to, children }) => {
+  return (
+    <StyledLink to={to}>
+      {children}
+    </StyledLink>
+  );
+};
+
+BackLink.propTypes = {
+  to: PropTypes.object.isRequired,
+  children: PropTypes.string.isRequired,
+};
